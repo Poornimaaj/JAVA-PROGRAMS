af7cb9db924bce83357db0ad36fb8ad5a3cc65ed
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-18 15:49:00 +0530

    Update not_sum_8.java

--- ASSIGNMENTS/not_sum_8.java
+++ ASSIGNMENTS/not_sum_8.java
@@ -3,22 +3,25 @@ class Main
 {
     public static void main(String args[])
     {
-        int i,sum=0;
-        for(i=0;i<9999;i++)
+        int i,sum,digit=0,temp;
+       
+        for(i=1;i<=10;i++)
         {
-            if((i<10) && (i!=8))
-            System.out.println(i);
-            
-            while(i>=10)
+            temp=i;
+           sum=0;
+            while(temp>0)
             {
-              i=i%10;
-              i=i/10;
-              sum=sum+i;
+                     digit = temp% 10;
+
+                  sum  = sum + digit;
+
+                    temp = temp/10;
+                   
             }
             if(sum!=8)
             System.out.println(i);
-        
-            
+       
+           
         }
     }
 }
