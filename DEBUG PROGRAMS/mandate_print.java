17a6ada9255ec13f634c48c72d61c0a90655c9fd
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-08 10:19:25 +0530

    Create mandate_print.java

---
+++ DEBUG PROGRAMS/mandate_print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+ 
+    try {
+      long data[] = new long[10000]; 
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+ 
+    
+      System.out.println("mandate");
+    
+    
+  }
+}
