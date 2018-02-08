e4f1476d1f04af76f8d24bd8d4115c6746128991
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-08 09:37:45 +0530

    Create exception.java

---
+++ DEBUG PROGRAMS/exception.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            System.out.print("1");
+            int sum = 9 / 0;
+            System.out.print("2");
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.print("3");
+        }
+        catch(Exception e)
+        {
+            System.out.print("4");
+        }
+        finally
+        {
+            System.out.print("5");
+        }
+    }
+}
