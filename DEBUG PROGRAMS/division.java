1cb8064e29fe67b5d1686b6761c05e033cefc1f9
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-08 09:58:21 +0530

    Create division.java

---
+++ DEBUG PROGRAMS/division.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+class Main {
+  public static void main(String[] args) {
+ 
+  int a, b, result;
+   Scanner input=new Scanner(System.in);
+    System.out.println("Input two integers");
+ 
+  a = input.nextInt();
+  b = input.nextInt();
+ 
+  result = a / b;
+ 
+  System.out.println("Result = " + result);
+  }
+}
