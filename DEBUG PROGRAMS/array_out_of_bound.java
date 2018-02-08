c99d7b09e1066e275face61980ab1b527651d617
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-08 10:23:36 +0530

    Create array_out_of_bound.java

---
+++ DEBUG PROGRAMS/array_out_of_bound.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+   public static void main(String args[]){
+      try{
+         int arr[]=new int[7];
+         arr[10]=10/5;
+         System.out.println("Last Statement of try block");
+      }
+     
+      catch(ArithmeticException e){
+         System.out.println("You should not divide a number by zero");
+      }
+      catch(ArrayIndexOutOfBoundsException e){
+         System.out.println("Accessing array elements outside of the limit");
+      }
+      System.out.println("Out of the try-catch block");
+   }
+}
